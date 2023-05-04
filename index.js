37919ca02ea9a4581395be2efc9538dd6cea8c0a
rashid <krashid4699@gmail.com>
2023-05-05 00:04:23 +0530

    lid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express()
 
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res)=>{
     res.json({message:"ok"})
